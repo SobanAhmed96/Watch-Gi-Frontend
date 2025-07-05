6fa752708a2f4a81f7f3bcbff91d78984a3deda4
SobanAhmed96 <sobanahmed9898@gmail.com>
2025-07-05 13:34:31 +0500

    watch gi

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -9,7 +9,8 @@ const Whatsapp = () => {
       href={`https://wa.me/${whatsappNumber}`}
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-12 right-6 bg-black-500 text-white rounded-full p-4 shadow-lg hover:bg-green-600 transition-colors duration-300"
+     className="fixed bottom-12 right-6 bg-black text-white rounded-full p-4 shadow-lg hover:bg-green-600 transition-colors duration-300"
+
     >
       <FaWhatsapp size={28} />
     </a>
